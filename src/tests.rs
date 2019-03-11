826688a6ff1654c76199511408a3d20b236526e2
Hans Ole Hatzel <hhatzel@gmail.com>
2019-03-11 19:41:59 +0100

    Add simple end-to-end tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,7 @@
-use crate::{FlashMessage, FlashResponse, Responder};
-use actix_web::{FromRequest, HttpResponse};
+use crate::{FlashMessage, FlashResponse, Responder, FlashMiddleware};
+use actix_web::{FromRequest, HttpResponse, App, http, HttpRequest};
 use actix_web::http::{Cookie, StatusCode};
-use actix_web::test::TestRequest;
+use actix_web::test::{TestRequest, TestServer};
 
 #[test]
 /// Ensure the response properly sets the `_flash` cookie.
@@ -37,4 +37,53 @@ fn bad_request() {
     assert!(err.downcast_ref::<serde_json::error::Error>().is_none());
     let resp = HttpResponse::from(err);
     assert_eq!(resp.status(), StatusCode::BAD_REQUEST)
+}
+
+fn minimal_app() -> App {
+    App::new()
+        .middleware(FlashMiddleware::default())
+        .route("/show_flash", http::Method::GET, show_flash)
+        .resource("/set_flash", |r| r.f(set_flash))
+}
+
+fn show_flash(flash: FlashMessage<String>) -> impl Responder {
+    flash.into_inner()
+}
+
+fn set_flash(_req: &HttpRequest) -> FlashResponse<HttpResponse, String> {
+    FlashResponse::new(
+        Some("This is the message".to_owned()),
+        HttpResponse::SeeOther()
+        .header(http::header::LOCATION, "/show_flash")
+        .finish(),
+        )
+}
+
+#[test]
+/// Integration test to assure the cookie is deleted on request
+fn cookie_is_set() {
+    let mut srv = TestServer::with_factory(minimal_app);
+
+    let req = srv.client(http::Method::GET, "/set_flash").finish().unwrap();
+    let resp = srv.execute(req.send()).unwrap();
+    assert_eq!(resp.status(), StatusCode::SEE_OTHER);
+
+    let cookie = resp.cookie("_flash").unwrap();
+    assert_eq!(cookie.value(), "\"This is the message\"");
+}
+
+#[test]
+/// Integration test to assure the cookie is deleted on request
+fn cookie_is_unset() {
+    let mut srv = TestServer::with_factory(minimal_app);
+    let req = srv.get().cookie(
+            http::Cookie::build("_flash", "To be deleted")
+            .path("/")
+            .finish()
+        ).finish().unwrap();
+    let resp = srv.execute(req.send()).unwrap();
+    println!("{:?}", resp);
+    let cookie = resp.cookie("_flash").unwrap();
+    println!("Cookie: {:?}", cookie);
+    assert!(cookie.expires().unwrap() < time::now());
 }
